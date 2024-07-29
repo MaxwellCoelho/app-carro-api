51180f45d2676257b3d11f34553b5511ebbbe7ff
Maxwell Coelho <maxwell.coelho@globalhitss.com.br>
2024-07-29 18:12:25 -0300

    Ajuste na api de melhores para filtrar por ano

--- src/services/bestService.ts
+++ src/services/bestService.ts
@@ -11,6 +11,22 @@ export class BestService {
 
     public async getBestModels(pagination: any, filter?: any): Promise<any> {
         let apiFilter = filter ? this.utils.convertIdToObjectId(filter) : {};
+        const yearEndFilter = 'generation.g1.yearEnd';
+        const qtdGens = 15;
+
+        if (Object.keys(apiFilter).includes(yearEndFilter)) {
+            const content = apiFilter[yearEndFilter];
+            delete apiFilter[yearEndFilter];
+
+            let rangeFilter = {$or: []}
+
+            for (let i = 1; i <= qtdGens; i++) {
+                rangeFilter.$or.push({[`generation.g${i}.yearEnd`]: content});
+            }
+
+            apiFilter = {...apiFilter, ...rangeFilter};
+        }
+
         let myFilter = { review: false, active: true, val_length: { $gt: 0 }, ...apiFilter };
         let mySort = { average: 'desc', _id: 'desc' };
         let res;
